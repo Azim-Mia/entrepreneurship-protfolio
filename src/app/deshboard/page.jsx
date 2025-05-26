d2ea957ea9be1caa9f67ed44a98550fa5e8a01c9
azim-mia <mdazimmia01980@gmail.com>
2025-05-26 23:27:28 +0600

    add deshboard menu

---
+++ src/app/deshboard/page.jsx
@@ -0,0 +1,8 @@
+import DeshboardNav from '@/components/deshboard/DeshboardNav'
+const Dashboard = () => {
+  return (<>
+    <DeshboardNav />
+  </>);
+};
+
+export default Dashboard;
---
+++ src/components/deshboard/DeshboardNav.jsx
@@ -0,0 +1,23 @@
+const DeshboardNav =()=>{
+  return (<>
+        <div className="min-h-screen flex bg-gray-100 p-4">
+
+      {/* Sidebar */}
+      <aside className="w-64 bg-white rounded-2xl shadow-lg p-6 space-y-4">
+        <h1>আমাদের সেবা</h1>
+        <nav className="space-y-2">
+          <strong className="block text-xl font-semibold text-blue-600 mb-4">
+       ড্যাশবোর্ড পরিচিতি     
+          </strong>
+  
+      </nav>
+      </aside>
+
+      {/* Main Content */}
+      <main className="flex-1 ml-6">
+main
+      </main>
+    </div>
+  </>)
+}
+export default DeshboardNav
