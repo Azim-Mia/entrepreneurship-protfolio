a2816d381ae09d51e3c8a16b63b8aacc3b9da181
azim-mia <mdazimmia01980@gmail.com>
2025-05-31 21:41:36 +0600

    add

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,6 @@
 const Cart = ({ title, value, icon }) => {
   return (
-    <div className="flex justify-between items-center gap-6 bg-white p-4 rounded-lg shadow-md border-2 ring-1 ring-blue-100">
+    <div className="flex justify-between items-center gap-6 bg-white p-4 rounded-lg shadow-md border-l-4 border-b-4 border-pink">
       <div className="xs:text-sm font-semibold">
        <p className="xs:text-0.5xl text-blue-400">{title}</p>
         <p className="text-sm font-bold">{value}</p>
